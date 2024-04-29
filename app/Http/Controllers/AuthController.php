16a3c29acc857ccae5715cd8e99167c2ef2f410c
Bipin Joshi <bipin.ingrails@gmail.com>
2024-04-29 23:45:02 +0545

    first push

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,81 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function home()
+    {
+        if (Auth::check()) {
+            return view('home.homepage');
+        } else {
+            return redirect()->route('login');
+        }
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function showRegistrationForm()
+    {
+        return view('auth.signup');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->route('home');
+        }
+
+        return redirect()->back()->withInput()->withErrors(['email' => 'Invalid email or password.']);
+    }
+
+    public function register(Request $request)
+    {
+
+        try {
+            // Validate the request data
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:8|confirmed',
+            ]);
+        
+            // Create a new user
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+            ]);
+        
+            // Attempt to authenticate the user
+            Auth::attempt($request->only('email', 'password'));
+        } catch (ValidationException $e) {
+            // If a validation error occurs, catch the ValidationException
+            // and redirect back with the validation error messages
+            return redirect()->back()->withErrors($e->validator->getMessageBag())->withInput();
+        } catch (Exception $e) {
+            // If any other type of exception occurs, catch it and
+            // redirect back with the exception message
+            return redirect()->back()->withErrors($e->getMessage())->withInput();
+        }
+        
+        // If no exception occurred, redirect to the intended URL
+        return redirect()->route('home');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/', [AuthController::class, 'home'])->name('home');
+
+// Route::get('/', function () {
+//     return view('auth/signup');
+// });
+
+Route::get('register', [AuthController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [AuthController::class, 'register'])->name('registerStore');
+
+Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
